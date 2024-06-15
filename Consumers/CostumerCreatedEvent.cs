010af9ef55c84e249b9eee338beeb650ee161acf
Eduardo Casotti <casottiedu@live.com>
2024-06-15 18:06:30 -0300

    settings of message#

---
+++ Consumers/CostumerCreatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MassTransitTutorial.Consumers
+{
+    public interface CustomerCreatedEvent
+    {
+        string CustomerId { get; }
+        string Name { get; }
+        DateTime BirthDate { get; }
+        string Type { get; }
+        DateTime CreatedAt { get; }
+    }
+}
